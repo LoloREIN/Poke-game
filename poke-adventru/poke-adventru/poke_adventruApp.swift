fad8799df99f02177be4daafc678ad9eae1efb8d
Lorenzo Reinoso Fuentes <0212511@up.edu.mx>
2024-05-07 16:18:20 -0600

    File creation and first layout

---
+++ poke-adventru/poke-adventru/poke_adventruApp.swift
@@ -0,0 +1,17 @@
+//
+//  poke_adventruApp.swift
+//  poke-adventru
+//
+//  Created by Lorenzo Reinoso on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct poke_adventruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
