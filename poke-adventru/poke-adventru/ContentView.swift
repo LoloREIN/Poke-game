9afc1bd6573fd7d59ed62aed84a4c5f1c13b6f45
Lolo <0212511@up.edu.mx>
2024-06-07 17:08:36 -0600

    Add Structure

--- poke-adventru/poke-adventru/ContentView.swift
+++ poke-adventru/poke-adventru/ContentView.swift
@@ -1,55 +1,73 @@
-//
-//  ContentView.swift
-//  vistasPoke
-//
-//  Created by Samuel Martínez on 08/05/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var username: String = ""
     @State private var password: String = ""
+    @State private var shouldNavigate = false
+
     var body: some View {
-        ZStack{
-            VStack {
-                
-                Text("Iniciar sesión").fontWeight( .semibold).foregroundColor(.gray).opacity(0.4).font(.system(size: 40)).padding(.bottom,70).padding(.top,50)
-                HStack{
-                    Text("Username o Correo electrónico").fontWeight(.medium).foregroundColor(.gray).opacity(0.4).frame(alignment: .leading)
-                }.padding(.leading, -60)
-                TextField("ejemplo123@ola", text: $username)
-                                .padding()
-                                .background(Color(UIColor.systemGray6))
-                                .cornerRadius(20).padding(.horizontal, 20).padding(.bottom,20)
-                HStack{
-                    Text("Contraseña").fontWeight(.medium).foregroundColor(.gray).opacity(0.4)
-                   
-                }
-                .padding(.leading, -150)
-                SecureField("**********", text: $password)
-                                .padding()
-                                .background(Color(UIColor.systemGray6))
-                                .cornerRadius(20).padding(.horizontal, 20).padding(.bottom,20)
-                Spacer()
-                
-                Button("Iniciar sesión") {
-                       
-                            print("Botón presionado")
-                        }
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("Iniciar sesión")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
+                        .opacity(0.4)
+                        .font(.system(size: 40))
+                        .padding(.bottom, 70)
+                        .padding(.top, 50)
+
+                    TextField("ejemplo123@ola", text: $username)
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black)
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .cornerRadius(30)
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(20)
                         .padding(.horizontal, 20)
+                        .padding(.bottom, 20)
+
+                    SecureField("**********", text: $password)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(20)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 20)
+
+                    Spacer()
+
+                    Button("Iniciar sesión") {
+                        authenticateUser()
                     }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .cornerRadius(30)
+                    .padding(.horizontal, 20)
+
+                    // NavigationLink oculto para manejar la navegación de forma programática
+                    NavigationLink(destination: UserView(), isActive: $shouldNavigate) {
+                        EmptyView()
+                    }
+                }
             }
             .padding()
-        }}
+        }
+    }
+
+    func authenticateUser() {
+        // Aquí podrías añadir tu lógica de verificación
+        if username == "1" && password == "1" {
+            shouldNavigate = true
+        } else {
+            print("Credenciales incorrectas")
+        }
+    }
+}
 
 
-#Preview {
-    ContentView()
+// Vista de previsualización
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
