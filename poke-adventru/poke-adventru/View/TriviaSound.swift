22bd6589c210ced4770cbd769f724dc5910985ac
Lolo <0212511@up.edu.mx>
2024-06-10 15:05:25 -0600

    Add Sounds

---
+++ poke-adventru/poke-adventru/View/TriviaSound.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaSound.swift
+//  poke-adventru
+//
+//  Created by Lorenzo Reinoso on 10/06/24.
+//
+
+import SwiftUI
+
+struct TriviaSound: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaSound()
+}
---
+++ poke-adventru/poke-adventru/ViewModel/AudioPlayerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayerViewModel.swift
+//  poke-adventru
+//
+//  Created by Lorenzo Reinoso on 10/06/24.
+//
+
+import Foundation
